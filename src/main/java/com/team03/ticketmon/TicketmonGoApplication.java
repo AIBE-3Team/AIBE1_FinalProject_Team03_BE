d563d814c3aa31822d81d28fca8b39aafe93f924
uiseon98 <3dnjf14dlfzz@gmail.com>
2025-06-18 14:25:34 +0900

    feat:초기설정 및 연동설정

--- src/main/java/com/team03/ticketmon/TicketmonGoApplication.java
+++ src/main/java/com/team03/ticketmon/TicketmonGoApplication.java
@@ -2,12 +2,14 @@ package com.team03.ticketmon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class TicketmonGoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TicketmonGoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmonGoApplication.class, args);
+	}
 
 }

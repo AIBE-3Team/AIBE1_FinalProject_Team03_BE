1017b60acbd80bc377bb5dd49b5a0bb916fff88b
LeeWonGyu <bitamin707@naver.com>
2025-06-30 17:15:44 +0900

    fix: Booking 조회 시 N+1 불필요한 데이터 과다 조회 최적화 (JPQL join fetch 사용) 및 Booking DTO 수정

--- src/main/java/com/team03/ticketmon/user/service/MyBookingServiceImpl.java
+++ src/main/java/com/team03/ticketmon/user/service/MyBookingServiceImpl.java
@@ -1,5 +1,6 @@
 package com.team03.ticketmon.user.service;
 
+import com.team03.ticketmon.booking.domain.Booking;
 import com.team03.ticketmon.booking.service.BookingService;
 import com.team03.ticketmon.user.dto.UserBookingSummaryDTO;
 import jakarta.persistence.EntityNotFoundException;
@@ -29,15 +30,32 @@ public class MyBookingServiceImpl implements MyBookingService {
                         booking.getBookingNumber(),
                         booking.getConcert().getTitle(),
                         booking.getConcert().getConcertDate(),
+                        booking.getConcert().getVenueName(),
+                        booking.getConcert().getVenueAddress(),
                         booking.getStatus().toString(),
                         booking.getTotalAmount(),
-                        booking.getConcert().getPosterImageUrl()
+                        booking.getConcert().getPosterImageUrl(),
+                        getSeatList(booking)
                 ))
                 .toList();
     }
 
     @Override
     public void cancelBooking(Long userId, Long bookingId) {
+        // TODO. 예매 취소 로직 구현 예정
+    }
 
+    // 좌석 정보 가져오기
+    private List<String> getSeatList(Booking booking) {
+        return booking.getTickets().stream()
+                .map(ticket -> {
+                    var concertSeat = ticket.getConcertSeat();
+                    var seat = concertSeat.getSeat();
+                    return String.format("%s석 %s열 %d번",
+                            concertSeat.getGrade().name(),
+                            seat.getSeatRow(),
+                            seat.getSeatNumber()
+                    );
+                }).toList();
     }
 }
